01c36bcff4ae064a9cb0f31c3166797a1f449088
Mickael Belhassen <emoondev@gmail.com>
2021-12-03 08:08:11 +0200

    Initial Commit

---
+++ Bitakhon iOS/Bitakhon_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bitakhon_iOSApp.swift
+//  Bitakhon iOS
+//
+//  Created by Mickael Belhassen on 03/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct Bitakhon_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
